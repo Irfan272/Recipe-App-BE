1c87722dc47e16ff32183a8131d03735527ce6d6
Irfan Fadillah <900522@hq.bni.co.id>
2025-07-17 15:06:26 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,87 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+import { db } from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
+import { eq, and } from "drizzle-orm";
+
+const app = express();
+const PORT = ENV.PORT || 5001;
+
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
+
+app.get("/api/favorites/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const userFavorites = await db
+      .select()
+      .from(favoritesTable)
+      .where(eq(favoritesTable.userId, userId));
+
+      if (userFavorites.length === 0) {
+      return res.status(404).json({ message: "No favorites found for this user." });
+    }
+
+    res.status(200).json(userFavorites);
+  } catch (error) {
+    console.log("Error feching the favorites", error);
+    req.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/api/favorites", async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const newFavorites = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json(newFavorites[0]);
+  } catch (error) {
+    console.log("Error adding favorite", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    const result = await db
+      .delete(favoritesTable)
+      .where(
+        and(
+          eq(favoritesTable.userId, userId),
+          eq(favoritesTable.recipeId, parseInt(recipeId))
+        )
+      );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Favorite not found" });
+    }
+    res.status(200).json({ message: "Favorite removed successfully" });
+  } catch (error) {
+    console.log("Error removing a favorite", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is runnin on PORT:", PORT);
+});
